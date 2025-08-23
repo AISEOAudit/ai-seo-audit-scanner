f7bd6cfe2e2a582c895b64ed8dfa2a97e15b7e60
AISEOAudit <info@aiseoaudit.io>
2025-08-23 19:24:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const USER_AGENTS = {
   Googlebot: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
   'Google-Extended':
     'Mozilla/5.0 (compatible; Google-Extended/1.0; +https://developers.google.com/search/blog/2023/09/google-extended)',
-  Default: 'Mozilla/5.0 (AI Visibility Scanner; +https://example.com/ai-scanner)'
+  Default: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 };
 
 // Simple concurrency limiter
